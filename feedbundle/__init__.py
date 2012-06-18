c372823ec2d96cde0eecd8d108deb78ce8dc8628
lepture <lepture@me.com>
2012-06-19 02:51:29 +0800

    init setup

---
+++ feedbundle/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+__version__ = '1.0.0'
+__author__ = 'Hsiaoming Yang <lepture@me.com>'
+__homepage__ = 'https://github.com/pythoncn/feedbundle'
--- setup.py
+++ setup.py
@@ -2,7 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import sys
-kwargs = {}
 major, minor = sys.version_info[:2]
+kwargs = {}
+kwargs['include_package_data'] = True
 if major >= 3:
     kwargs['use_2to3'] = True
+
+from setuptools import setup
+install_requires = ['Flask']
+try:
+    import argparse  # python 2.7+ support argparse
+except ImportError:
+    install_requires.append('argparse')
+
+
+import feedbundle
+from email.utils import parseaddr
+author, author_email = parseaddr(feedbundle.__author__)
+
+setup(
+    name='feedbundle',
+    version=feedbundle.__version__,
+    author=author,
+    author_email=author_email,
+    url=feedbundle.__homepage__,
+    packages=['feedbundle'],
+    description='FeedBundle',
+    license='BSD License',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+    ],
+    **kwargs
+)
